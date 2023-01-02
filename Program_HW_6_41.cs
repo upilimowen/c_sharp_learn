7865c6ede4903a86aa312c55ade1570e806a6934
upilimowen <117759906+upilimowen@users.noreply.github.com>
2023-01-02 08:32:59 +0300

    Домашняя работа 6

---
+++ Program_HW_6_41.cs
@@ -0,0 +1,48 @@
+﻿string MakeStringFromArray(int[] array)
+{
+    string result = ""; 
+        int size = array.Length;
+    int index = 0;
+    while (index < size)
+    {
+        result = result + array[index] + ", ";
+        index = index + 1;            
+    }
+    int resultSize = result.Length - 2;
+    result = result.Substring(0, resultSize);
+
+    return result;
+}
+
+string answer = "";
+//использование массивов - не самое оптимальный способ решения этой задачи,
+//но так как тема урока называется "Одномерные массивы. Продолжение" то работаю с массивом
+int[] array = new int[0];
+int counter = 0;
+int number = 0;
+
+while (true)
+{
+    Console.WriteLine("Введите, пожалуйста, целое число. Если захотите прерваться - введите Q");
+    answer = Console.ReadLine();
+    if ((answer == "Q")|(answer == "q"))
+        {
+            break;
+        }
+    try
+    {
+        number = Convert.ToInt32(answer);
+    }
+    catch
+    {
+        Console.WriteLine("Извините, не понял?");    
+    }
+    Array.Resize(ref array, array.Length + 1);
+    array[array.Length-1] = number;
+    if (number > 0)
+    {
+        counter = counter + 1;
+    }
+}
+string result = MakeStringFromArray(array); 
+Console.WriteLine(result + " -> " + counter); 
---
+++ Program_HW_6_43.cs
@@ -0,0 +1,30 @@
+﻿//использование массивов - не самое оптимальный способ решения этой задачи,
+//но так как тема урока называется "Одномерные массивы. Продолжение" то работаю с массивом
+int[] array1 = new int[2];
+int[] array2 = new int[2];
+
+Console.WriteLine("Введите, пожалуйста, коэффициенты k и b уравнения y = k*x + b для первого уравнения");
+array1[0] = Convert.ToInt32(Console.ReadLine());
+array1[1] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Спасибо. Введите, пожалуйста, коэффициенты k и b уравнения y = k*x + b для второго уравнения");
+array2[0] = Convert.ToInt32(Console.ReadLine());
+array2[1] = Convert.ToInt32(Console.ReadLine());
+
+if  (array1[0] == array2[0])
+{ 
+    if(array1[1] == array2[1])
+    {
+        Console.WriteLine("Это две идентичные прямые");
+    }
+    else
+    {
+        Console.WriteLine("Эти прямые не пересекаются");
+    }
+}    
+else
+{
+    double resultX = (array2[1] - array1[1])/(array1[0] - array2[0]);
+    double resultY = array1[0] * resultX + array1[1];
+    Console.WriteLine("Точка пересечения этих прямых (" + resultX + ";" +resultY + ")");
+}
